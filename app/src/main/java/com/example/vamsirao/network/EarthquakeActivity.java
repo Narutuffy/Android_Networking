4833e38728997972989cf70f9ed6301aa5d3f401
Vamsi <s.vamsirao@gmail.com>
2017-02-16 08:43:55 +0530

    Take an example JSON response and extract the required data

--- app/src/main/java/com/example/vamsirao/network/EarthquakeActivity.java
+++ app/src/main/java/com/example/vamsirao/network/EarthquakeActivity.java
@@ -19,14 +19,14 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("7.2","San Francisco","Feb 2, 2017"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        /*earthquakes.add(new Earthquake("7.2","San Francisco","Feb 2, 2017"));
         earthquakes.add(new Earthquake("6.1","London","Jan 14, 2017"));
         earthquakes.add(new Earthquake("8.0","Lahore","Dec 15 , 2016"));
         earthquakes.add(new Earthquake("7.7","Mexico City","Dec 2, 2016"));
         earthquakes.add(new Earthquake("12.2","Trump Plaza","Nov 4, 2016"));
         earthquakes.add(new Earthquake("5.2","Paris","Dec 10, 2016"));
-
+*/
 
 
 
