7728174869d77073c345fb4ee2e8081ec8e63881
Vamsi <s.vamsirao@gmail.com>
2017-02-27 18:26:42 +0530

    Add Settings fragment

---
+++ app/src/main/java/com/example/vamsirao/network/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.vamsirao.network;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+
+    }
+}
